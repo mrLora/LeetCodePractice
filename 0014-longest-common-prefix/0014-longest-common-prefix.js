859e7f9c0d96acc60a3400995e3e68eae285ef64
Lora <jonathan.lora@protonmail.com>
2024-02-04 09:53:42 -0500

    Time: 53 ms (72.64%), Space: 49.3 MB (17.77%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+const longestCommonPrefix = (strs) => {
+  let prefix = '';
+  if (strs.length === 0) return prefix;
+
+  for (let i = 0; i < strs[0].length; i++) {
+    const character = strs[0][i]
+    for (let j = 0; j < strs.length; j++) {
+      if (strs[j][i] !== character) return prefix;
+    }
+    prefix = prefix + character;
+  }
+  return prefix;
+};
