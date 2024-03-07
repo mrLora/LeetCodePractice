f4c800381f6423a24a3a48d1045167828a3ce752
Lora <jonathan.lora@protonmail.com>
2024-03-07 00:02:24 -0500

    Time: 77 ms (23.66%), Space: 52.2 MB (55.27%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfFour = function(n) {
+    if (n <= 0) {
+        return false;
+    }
+
+    while (n !== 1) {
+        if (n % 4 !== 0) {
+            return false;
+        }
+        n = n / 4;
+    }
+
+    return true;
+};
